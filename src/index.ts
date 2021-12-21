f7ae1a7d0312b1d86b13172cd1f71f13cde509b6
Juunini (쥬니니) <juuni.ni.i@gmail.com>
2021-12-22 08:16:14 +0900

    Create index.ts

---
+++ src/index.ts
@@ -0,0 +1,3 @@
+export { default as stereoscopic } from './stereoscopic';
+export { default as StereoscopicCamera } from './components/StereoscopicCamera';
+export { default as StereoscopicVideo } from './components/StereoscopicVideo';
